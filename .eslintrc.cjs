34b8891bf1270275840e2dfe6738e28f59d962b4
angelop47 <pescettoangelo@gmail.com>
2026-01-10 15:37:42 -0300

    chore: add and update project config files (ESLint, Prettier, Astro, package) for consistent development setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:astro/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+  ],
+  plugins: ["jsx-a11y"],
+};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://example.com",
+    integrations: [sitemap()],
+    prefetch: {
+        prefetchAll: true,
+        defaultStrategy: "viewport",
+    },
+    build: {
+        inlineStylesheets: "auto",
+    },
+});
