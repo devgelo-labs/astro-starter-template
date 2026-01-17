e4db8aaaef80c23733f4dda6161c7283c8a62124
angelop47 <pescettoangelo@gmail.com>
2026-01-17 10:20:50 -0300

    feat: centralize navigation and social links in siteConfig

--- src/config/navigation.ts
+++
@@ -1,9 +0,0 @@
-import type { NavLink } from "../types/types";
-
-export const navLinks: NavLink[] = [
-  { text: "Inicio", href: "/" },
-  { text: "Nosotros", href: "/about" },
-  { text: "Servicios", href: "/services" },
-  { text: "Blog", href: "/blog" },
-  { text: "Contacto", href: "/contact" },
-];
--- src/config/site.ts
+++ src/config/site.ts
@@ -8,4 +8,16 @@ export const siteConfig = {
   author: "Devgelo",
   twitter: "@Devgelo",
   ogImage: ogImage,
+  socialLinks: {
+    twitter: "https://twitter.com",
+    github: "https://github.com/angelop47/astro-starter-template",
+    discord: "https://discord.com",
+  },
+  navLinks: [
+    { text: "Inicio", href: "/" },
+    { text: "Nosotros", href: "/about" },
+    { text: "Servicios", href: "/services" },
+    { text: "Blog", href: "/blog" },
+    { text: "Contacto", href: "/contact" },
+  ],
 };
